ec5b4b2348da3a9191258c396eb4b3add066bd1a
Max Veregge <maxwell.veregge@gmail.com>
2020-08-03 20:58:41 -0400

    implement read, create and update for matchdays

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var express = require("express");
 var mysql = require("./dbcon.js");
+var moment = require("moment");
 
 var app = express();
 var handlebars = require("express-handlebars").create({
+  helpers: {
+    selected: selected,
+  },
   defaultLayout: "main",
 });
+
+var bodyParser = require("body-parser");
 
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 app.set("port", 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/index", function (req, res, next) {
   var context = {};
@@ -31,16 +40,64 @@ app.get("/players", function (req, res, next) {
 });
 
 app.get("/matchdays", function (req, res, next) {
-  var context = {};
-  context.title = "Matchdays";
-  mysql.pool.query("SELECT * FROM Matchdays", function (err, result) {
+  var context = { scripts: ["matchdays.js"], title: "Matchdays" };
+  mysql.pool.query(
+    "SELECT Matchdays.matchdayId, Matchdays.gameDate, Teams1.teamName AS teamName1,\
+    Teams1.teamId AS teamId1, Teams2.teamName AS teamName2, Teams2.teamId AS teamId2\
+    FROM Matchdays JOIN Teams AS Teams1 ON Matchdays.teamId1 = Teams1.teamId JOIN Teams AS Teams2 ON Matchdays.teamId2 = Teams2.teamId",
+    function (err, result) {
+      if (err) {
+        next(err);
+      }
+      context.matchdays = result;
+      context.matchdays.map((m) => {
+        m.gameDate = moment(m.gameDate).format("yyyy-MM-DDTHH:mm");
+        return m;
+      });
+      mysql.pool.query("SELECT teamName, teamId FROM Teams", function (
+        err,
+        result
+      ) {
+        if (err) {
+          next(err);
+        }
+        context.teams = result;
+        res.render("matchdays", context);
+      });
+    }
+  );
+});
+
+app.post("/newMatchday", function (req, res) {
+  let values = [];
+  for (let val in req.body) {
+    values.push(req.body[val]);
+  }
+  const insertString =
+    "INSERT INTO Matchdays (teamId1, teamId2, gameDate) VALUES (?, ?, ?)";
+  mysql.pool.query(insertString, values, function (err, results) {
     if (err) {
+      res.send(err);
       next(err);
       return;
     }
-    console.log(result);
-    context.results = result;
-    res.render("matchdays", context);
+    res.redirect("matchdays");
+  });
+});
+
+app.put("/updateMatchday", function (req, res) {
+  let values = [];
+  for (let val in req.body) {
+    values.push(req.body[val]);
+  }
+  const updateString =
+    "UPDATE Matchdays SET gameDate=?, teamId1=?, teamId2=? WHERE matchdayId=?";
+  mysql.pool.query(updateString, values, function (err, results) {
+    if (err) {
+      res.send(err);
+    }
+    res.send("success");
+    return;
   });
 });
 
@@ -195,3 +252,12 @@ app.listen(app.get("port"), function () {
       "; press Ctrl-C to terminate."
   );
 });
+
+//Handlebars helpers
+function selected(option, value) {
+  if (option === value) {
+    return " selected";
+  } else {
+    return "";
+  }
+}
---
+++ public/scripts/matchdays.js
@@ -0,0 +1,104 @@
+function validateNewMatchday(form) {
+  let message;
+
+  const teamId1 = form.team1.value;
+  const teamId2 = form.team2.value;
+  const gameDate = form.gameDate.value;
+
+  const sameTeams = teamId1 === teamId2 && teamId1 !== "";
+  const blankTeam = teamId1 === "" || teamId2 === "";
+  const dateBeforeToday = new Date(gameDate) < new Date();
+
+  if (sameTeams) {
+    message = "Pick different teams!";
+  } else if (blankTeam) {
+    message = "Pick two teams!";
+  } else if (dateBeforeToday) {
+    message = "Pick a date after today!";
+  }
+
+  let messageBox = form.querySelector("span[name='message']");
+
+  if (message) {
+    messageBox.style.color = "red";
+    messageBox.innerHTML = message;
+    return false;
+  } else {
+    messageBox = "";
+    return true;
+  }
+}
+
+function validateExistingMatchday(matchday) {
+  let message;
+
+  const teamId1 = matchday.querySelector("select[name='team1']").value;
+  const teamId2 = matchday.querySelector("select[name='team2']").value;
+  const gameDate = matchday.querySelector("input[name='gameDate']").value;
+
+  const sameTeams = teamId1 === teamId2 && teamId1 !== "";
+  const blankTeam = teamId1 === "" || teamId2 === "";
+  const dateBeforeToday = new Date(gameDate) < new Date();
+
+  if (sameTeams) {
+    message = "Pick different teams!";
+  } else if (blankTeam) {
+    message = "Pick two teams!";
+  } else if (dateBeforeToday) {
+    message = "Pick a date after today!";
+  }
+
+  let messageBox = matchday.querySelector("span[name='message']");
+
+  if (message) {
+    messageBox.style.color = "red";
+    messageBox.innerHTML = message;
+    return false;
+  } else {
+    messageBox.innerHTML = "";
+    return true;
+  }
+}
+
+function editMatchday(ev) {
+  let matchday = ev.target.parentNode.parentNode;
+  let inputs = matchday.querySelectorAll("select, input");
+  for (let i = 0; i < inputs.length; ++i) {
+    inputs[i].disabled = false;
+  }
+  ev.target.removeEventListener("click", editMatchday);
+  ev.target.innerHTML = "Update";
+  ev.target.addEventListener("click", updateMatchday);
+}
+
+function updateMatchday(ev) {
+  let matchday = ev.target.parentNode.parentNode;
+  if (!validateExistingMatchday(matchday)) {
+    return;
+  }
+  let inputs = matchday.querySelectorAll("select, input");
+  let body = {};
+  for (let i = 0; i < inputs.length; ++i) {
+    body[inputs[i].name] = inputs[i].value;
+  }
+  $.ajax({
+    type: "PUT",
+    url: "updateMatchday",
+    contentType: "application/json",
+    data: JSON.stringify(body),
+  })
+    .done(function () {
+      for (let i = 0; i < inputs.length; ++i) {
+        inputs[i].disabled = true;
+      }
+      ev.target.removeEventListener("click", updateMatchday);
+      ev.target.innerHTML = "Edit";
+      ev.target.addEventListener("click", editMatchday);
+    })
+    .fail(function () {
+      alert("Update failed! Try again.");
+    });
+}
+
+let edits = document.querySelectorAll("button[name='editMatchday']");
+edits.forEach((edit) => edit.addEventListener("click", editMatchday));
