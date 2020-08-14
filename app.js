c4103f87d1b0778b382589a53f2901a0bc64b036
Max Veregge <maxwell.veregge@gmail.com>
2020-08-13 21:31:35 -0400

    idk

--- app.js
+++ app.js
@@ -416,11 +416,9 @@ app.use(function (err, req, res, next) {
 });
 
 app.listen(app.get("port"), function () {
-  console.log(
-    "Express started on http://localhost:" +
-      app.get("port") +
-      " press Ctrl-C to terminate."
-  );
+  "Express started on http://localhost:" +
+    app.get("port") +
+    " press Ctrl-C to terminate.";
 });
 
 //Handlebars helpers
